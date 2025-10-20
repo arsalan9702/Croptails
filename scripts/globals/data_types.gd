fe2fb5078f94b646c33135672bdb2ad959b7aa50
arsalan <arsalansayed9702@gmail.com>
2025-10-20 20:47:31 +0530

    crops growth complete

--- scripts/globals/data_types.gd
+++ scripts/globals/data_types.gd
@@ -1,10 +1,19 @@
 class_name DataTypes
 
-enum Tools{
+enum Tools {
 	None,
 	AxeWood,
 	TillGround,
 	WaterCrops,
 	PlantCorn,
-	PlantTomato	
+	PlantTomato
+}
+
+enum GrowthStates {
+	Seed,
+	Germination,
+	Vegetative,
+	Reproduction,
+	Maturity,
+	Harvesting
 }
