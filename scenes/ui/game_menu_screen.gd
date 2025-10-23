466f0b0e71adf21d8144e36d422fdcd01b42a778
arsalan <arsalansayed9702@gmail.com>
2025-10-23 15:03:54 +0530

    game menu done, saving left

--- scenes/ui/game_menu_screen.gd
+++ scenes/ui/game_menu_screen.gd
@@ -4,7 +4,8 @@ extends CanvasLayer
 
 func _ready() -> void:
 	save_game_button.disabled = !SaveGameManager.allow_save_game
-	save_game_button.focus_mode = SaveGameManager.allow_save_game if Control.FOCUS_ALL else Control.FOCUS_NONE 
+	#save_game_button.focus_mode = SaveGameManager.allow_save_game if Control.FOCUS_ALL else Control.FOCUS_NONE 
+	save_game_button.focus_mode = Control.FOCUS_ALL if SaveGameManager.allow_save_game else Control.FOCUS_NONE
 
 func _on_start_game_button_pressed() -> void:
 	GameManager.start_game()
